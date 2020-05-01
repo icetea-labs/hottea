a96e543b16aea5efdee3c774f21b1cf64d91d54b
Truong Hong Thi <thi1981@gmail.com>
2020-05-02 02:20:32 +0700

    Adjust cli port

--- cli/index.js
+++ cli/index.js
@@ -1,16 +1,11 @@
 #!/usr/bin/env node
 
 const { version } = require('../package.json')
-const path = require('path')
-const { spawn } = require('child_process')
-const debugFactory = require('debug')
-debugFactory.enable('hottea*')
-const debug = debugFactory('hottea')
 const program = require('commander')
 const handler = require('serve-handler');
 const http = require('http');
 
-const launchWeb = async (options) => {
+const launchWeb = async (cmd) => {
   const server = http.createServer((request, response) => {
     // You pass two more arguments for config and middleware
     // More details here: https://github.com/zeit/serve-handler#options
@@ -19,22 +14,15 @@ const launchWeb = async (options) => {
     });
   })
 
-  const port = (options && options.port) ? options.port : 3001
+  const port = (cmd && cmd.port) ? cmd.port : 3001
   server.listen(port, () => {
-    console.log('Running at http://localhost:3001');
+    console.log('Running at http://localhost:' + port);
   });
 }
 
-if (process.argv.length < 3) {
-  launchWeb()
-} else {
-  program.version(version)
-  program
-    .command('web')
-    .description('start a blockchain web for development')
-    .option('-p, --port <port>', 'web server port, default to 3001')
-    .action(launchWeb)
-    
-  program.parse(process.argv)
-}
-
+program.version(version)
+program
+  .option('-p, --port <port>', 'web server port')
+  .action(launchWeb)
+  
+program.parse(process.argv)
